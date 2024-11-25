389c5c6da25ef9b06d85ccdaa338fc37e4202396
neoki <ot07neoki@gmail.com>
2024-11-25 20:56:00 +0900

    refactor: svelte code format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  import { formatDate } from '$lib/date'
-  import * as config from '$lib/config'
+	import { formatDate } from '$lib/date';
+	import * as config from '$lib/config';
 
-  let { data } = $props()
+	let { data } = $props();
 </script>
 
 <svelte:head>
-  <title>{config.title}</title>
+	<title>{config.title}</title>
 </svelte:head>
 
 <sction>
-  <ul>
-    {#each data.posts as post}
-      <li>
-        <a href={post.slug}>{post.title}</a>
-        <p>{formatDate(post.date)}</p>
-        <p>{post.description}</p>
-      </li>
-    {/each}
-  </ul>
+	<ul>
+		{#each data.posts as post}
+			<li>
+				<a href={post.slug}>{post.title}</a>
+				<p>{formatDate(post.date)}</p>
+				<p>{post.description}</p>
+			</li>
+		{/each}
+	</ul>
 </sction>
