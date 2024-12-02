582c4aaee1cfeed6c1836f0499157a4666e5f6e6
neoki <ot07neoki@gmail.com>
2024-12-02 21:14:47 +0900

    fix: update post page styles

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { formatDate } from '$lib/date';
+	import * as config from '$lib/config';
 
 	let { data } = $props();
 </script>
@@ -10,9 +11,54 @@
 	<meta property="og:title" content={data.post.title} />
 </svelte:head>
 
-<article>
-	<h1>{data.post.title}</h1>
-	<p>{formatDate(data.post.date)}</p>
-	<p>{data.post.description}</p>
-	<div>{@html data.post.content}</div>
-</article>
+<header>
+	<a href="/">{config.title}</a>
+</header>
+<section>
+	<article>
+		<h1>{data.post.title}</h1>
+		<p class="published-date">{formatDate(data.post.date)}</p>
+		<div class="content">{@html data.post.content}</div>
+	</article>
+</section>
+
+<style>
+	header {
+		text-align: center;
+		padding-block: 1rem;
+		padding-inline: 2rem;
+	}
+
+	header a {
+		font-size: 1rem;
+		font-weight: bold;
+		line-height: 1;
+	}
+
+	section {
+		display: flex;
+		justify-content: center;
+		margin-block-start: 2rem;
+	}
+
+	article {
+		padding-inline: 2rem;
+		max-width: 40rem;
+	}
+
+	h1 {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+
+	.published-date {
+		margin-block-start: 0.5rem;
+		font-size: 0.875rem;
+		font-weight: 500;
+		color: #374151;
+	}
+
+	.content {
+		margin-block-start: 2rem;
+	}
+</style>
